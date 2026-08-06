71c08b90f6f20e99c4f386a5c88861b48015101d
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2026-08-06 13:47:47 -0700

    loaders set up for project index/details

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,8 +1,27 @@
-const ProjectsPage = () => {
+import ProjectCard from "~/components/ProjectCard";
+import type {Route} from "./+types/index" // loader requires this route type
+import type {Project} from '~/types';
+
+
+export async function loader({request}:Route.LoaderArgs): Promise<{projects: Project[]}> { // must be lowercase. our request will be in the shape of route.loaderargs, and the function will return a promise object with an array of project objects
+    const res = await fetch('http://localhost:8000/projects')
+    const data = await res.json();
+    return {projects: data} //make a new object with the data
+}
+
+const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
+
+    const {projects} = loaderData as {projects: Project []};
+
     return ( <>
-        <h2 className="text-3xl text-white font-bold">
+        <h2 className="text-3xl text-white font-bold pb-5">
             🚀 Projects
         </h2>
+        <div className="grid gap-6 sm:grid-cols-2">
+            {projects.map((project) => (
+                <ProjectCard key={project.id} project={project}/>
+            ))}
+        </div>
     </> );
 }
  
